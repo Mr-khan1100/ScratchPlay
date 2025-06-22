92fd40b5b5695550347b778f0ada635d9da423d4
Habib <hk6632521@gmail.com>
2025-06-22 21:49:12 +0530

    added path library

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,18 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-
+const path = require("path");
 process.env["NODE_ENV"] = "production";
 
 module.exports = merge([
   common,
   {
     mode: "production",
+    output: { // Add this output configuration
+      path: path.resolve(__dirname, "build"), // Creates a 'build' folder
+      filename: "app.js",
+      clean: true, // Cleans the output directory before emit
+    },
     optimization: {
       minimize: true,
       minimizer: [
